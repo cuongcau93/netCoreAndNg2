8c7012c532b7e926c6e53412b8fb41c9400873ca
cuongcau93 <cuongnm22@wru.vn>
2017-05-29 17:23:03 +0700

    Watching ,Reacting, Reactive Transformations

--- AngularProjectDotnetCore/AngularProjectDotnetCore/wwwroot/app/customer/customer.component.js
+++ AngularProjectDotnetCore/AngularProjectDotnetCore/wwwroot/app/customer/customer.component.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var customer_1 = require("./customer");
 var forms_1 = require("@angular/forms");
+require("rxjs/add/operator/debounceTime");
 //function ratingRange(c: AbstractControl): { [key: string]: boolean | null } {
 //    if (c.value !== undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
 //        return { 'range': true };
@@ -20,16 +21,39 @@ var forms_1 = require("@angular/forms");
 //}
 function ratingRange(min, max) {
     return function (c) {
-        if (c.value !== undefined && (isNaN(c.value)) || c.value < 1 || c.value > 5) {
+        if (c.value !== undefined && (isNaN(c.value)) || c.value < min || c.value > max) {
             return { 'range': true };
         }
         return null;
     };
 }
+function emailMatcher(c) {
+    var emailControl = c.get('email');
+    var confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return { 'match': true };
+}
 var CustomerComponent = (function () {
     function CustomerComponent(fb) {
         this.fb = fb;
         this.customer = new customer_1.Customer();
+        this.validationMessage = {
+            required: 'Please enter your email address.',
+            pattern: 'Please enter a valid email address.'
+        };
+        this.validationMessageFirstName = {
+            required: 'Please enter your first name.',
+            minlength: 'The first name must be longer than 3 characters.'
+        };
+        this.validationMessageRating = {
+            required: 'Please enter your first name.',
+            minlength: 'The first name must be longer than 3 characters.'
+        };
     }
     CustomerComponent.prototype.testData = function () {
         //set value parameter full
@@ -45,14 +69,34 @@ var CustomerComponent = (function () {
         });
     };
     CustomerComponent.prototype.ngOnInit = function () {
+        var _this = this;
         this.customerForm = this.fb.group({
             firstName: ['', [forms_1.Validators.required, forms_1.Validators.minLength(3)]],
             lastName: ['', [forms_1.Validators.required, forms_1.Validators.minLength(3)]],
-            email: ['', [forms_1.Validators.required, forms_1.Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
+            emailGroup: this.fb.group({
+                email: ['', [forms_1.Validators.required, forms_1.Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
+                confirmEmail: ['', forms_1.Validators.required],
+            }, { validator: emailMatcher }),
             phone: ['', [forms_1.Validators.required]],
             notification: 'email',
             rating: ['', ratingRange(1, 5)],
             sendCatalog: true
+        });
+        this.customerForm.get('notification').valueChanges
+            .subscribe(function (value) { return _this.setNotification(value); });
+        this.customerForm.get('firstName').valueChanges
+            .subscribe(function (value) { return console.log(value); });
+        var emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.subscribe(function (value) {
+            _this.setMessage(emailControl);
+        });
+        var firstNameControl = this.customerForm.get('firstName');
+        firstNameControl.valueChanges.debounceTime(1000).subscribe(function (value) {
+            _this.setMessageFirstName(firstNameControl);
+        });
+        var ratingControl = this.customerForm.get('rating');
+        ratingControl.valueChanges.subscribe(function (value) {
+            _this.setMessageRating(ratingControl);
         });
         //this.customerForm = new FormGroup({
         //    firstName: new FormControl(),
@@ -73,7 +117,6 @@ var CustomerComponent = (function () {
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     };
     CustomerComponent.prototype.setNotification = function (notifyVia) {
-        console.log(notifyVia);
         var phoneControl = this.customerForm.get('phone');
         if (notifyVia === 'text') {
             phoneControl.setValidators(forms_1.Validators.required);
@@ -82,6 +125,39 @@ var CustomerComponent = (function () {
             phoneControl.clearValidators();
         }
         phoneControl.updateValueAndValidity();
+    };
+    CustomerComponent.prototype.setMessage = function (c) {
+        var _this = this;
+        this.emailMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(function (key) {
+                return _this.validationMessage[key];
+            }).join(' ');
+        }
+    };
+    CustomerComponent.prototype.setMessageFirstName = function (c) {
+        var _this = this;
+        this.emailMessageFirstName = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessageFirstName = Object.keys(c.errors).map(function (key) {
+                return _this.validationMessageFirstName[key];
+            }).join(' ');
+            (Object.keys(c.errors).map(function (key) {
+                return console.log('lol' + key);
+            }));
+        }
+    };
+    CustomerComponent.prototype.setMessageRating = function (c) {
+        var _this = this;
+        this.emailMessageRating = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessageRating = Object.keys(c.errors).map(function (key) {
+                return _this.validationMessageRating[key];
+            }).join(' ');
+            (Object.keys(c.errors).map(function (key) {
+                return console.log('lol' + key);
+            }));
+        }
     };
     return CustomerComponent;
 }());
--- AngularProjectDotnetCore/AngularProjectDotnetCore/wwwroot/app/customer/customer.component.ts
+++ AngularProjectDotnetCore/AngularProjectDotnetCore/wwwroot/app/customer/customer.component.ts
@@ -1,6 +1,8 @@
 ﻿import { Component, OnInit } from "@angular/core";
 import { Customer } from "./customer";
-import { FormGroup, FormBuilder, FormControl, Validators, AbstractControl, ValidatorFn } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from "@angular/forms";
+
+import 'rxjs/add/operator/debounceTime';
 
 //function ratingRange(c: AbstractControl): { [key: string]: boolean | null } {
 //    if (c.value !== undefined && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
@@ -18,6 +20,18 @@ function ratingRange(min: number, max: number): ValidatorFn {
     };
 }
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean | null } {
+    let emailControl = c.get('email');
+    let confirmControl = c.get('confirmEmail');
+    if (emailControl.pristine || confirmControl.pristine) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return {'match': true} ;
+}
+
 @Component({
     moduleId: module.id,
     templateUrl: 'customer.component.html',
@@ -27,7 +41,23 @@ export class CustomerComponent implements OnInit {
 
     customerForm: FormGroup;
     customer: Customer = new Customer();
+    emailMessage: string;
+    emailMessageFirstName: string;
+    emailMessageRating: string;
+    private validationMessage = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address.'
+    };
 
+    private validationMessageFirstName = {
+        required: 'Please enter your first name.',
+        minlength: 'The first name must be longer than 3 characters.'
+    };
+
+    private validationMessageRating = {
+        required: 'Please enter your first name.',
+        minlength: 'The first name must be longer than 3 characters.'
+    };
     constructor(private fb: FormBuilder) { }
 
     testData(): void {
@@ -46,16 +76,44 @@ export class CustomerComponent implements OnInit {
     }
 
     ngOnInit(): void {
+
         this.customerForm = this.fb.group({
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['', [Validators.required, Validators.minLength(3)]],
-            email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+")]],
+                confirmEmail: ['', Validators.required],
+            }, {validator: emailMatcher}),
             phone: ['', [Validators.required]],
             notification: 'email',
             rating: ['', ratingRange(1, 5)],
             sendCatalog: true
         });
 
+        this.customerForm.get('notification').valueChanges
+            .subscribe(value => this.setNotification(value));
+
+        this.customerForm.get('firstName').valueChanges
+            .subscribe(value => console.log(value));
+
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.subscribe(value => {
+                this.setMessage(emailControl);
+            }
+        );
+
+        const firstNameControl = this.customerForm.get('firstName');
+        firstNameControl.valueChanges.debounceTime(1000).subscribe(value => {
+            this.setMessageFirstName(firstNameControl);
+        }
+        );
+
+        const ratingControl = this.customerForm.get('rating');
+        ratingControl.valueChanges.subscribe(value => {
+            this.setMessageRating(ratingControl);
+        }
+        );
+        
         //this.customerForm = new FormGroup({
         //    firstName: new FormControl(),
         //    lastName: new FormControl(),
@@ -78,7 +136,6 @@ export class CustomerComponent implements OnInit {
     }
 
     setNotification(notifyVia: string): void {
-        console.log(notifyVia);
         const phoneControl = this.customerForm.get('phone');
         if (notifyVia === 'text') {
             phoneControl.setValidators(Validators.required);
@@ -86,6 +143,34 @@ export class CustomerComponent implements OnInit {
             phoneControl.clearValidators();
         }
         phoneControl.updateValueAndValidity();
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key =>
+                this.validationMessage[key]).join(' ');     
+        }
+    }
+
+    setMessageFirstName(c: AbstractControl): void {
+        this.emailMessageFirstName = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessageFirstName = Object.keys(c.errors).map(key => 
+                this.validationMessageFirstName[key]).join(' ');
+           (Object.keys(c.errors).map(key =>
+                console.log('lol'+key)));
+        }
+    }
+
+    setMessageRating(c: AbstractControl): void {
+        this.emailMessageRating = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessageRating = Object.keys(c.errors).map(key =>
+                this.validationMessageRating[key]).join(' ');
+            (Object.keys(c.errors).map(key =>
+                console.log('lol' + key)));
+        }
     }
 
 }
